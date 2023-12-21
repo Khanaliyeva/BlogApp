0aa5aa2e485a39121e0797fdca120a7736ef1d0d
Khanaliyeva <gunelxaneliyeva@gmail.com>
2023-12-21 18:59:43 +0400

    Add project files.

---
+++ BlogApp.Business/Services/Implimentations/BrandService.cs
@@ -0,0 +1,29 @@
+﻿using BlogApp.Business.Services.Interfaces;
+using BlogApp.Core.Entities;
+using BlogApp.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Business.Services.Implimentations
+{
+    public class BrandService:IBrandService
+    {
+        private readonly IBrandRepository _repo;
+
+        public BrandService(IBrandRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<ICollection<Brand>> GetAllAsync()
+        {
+           var brand=await _repo.GetAllAsync();
+            return await brand.ToListAsync();
+        }
+        
+    }
+}
---
+++ BlogApp.DAL/Repositories/Implimentations/Repository.cs
@@ -0,0 +1,49 @@
+﻿using BlogApp.Core.Entities.Common;
+using BlogApp.DAL.Context;
+using BlogApp.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.DAL.Repositories.Implimentations
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity, new()
+    {
+        private readonly AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public DbSet<TEntity> Table =>_context.Set<TEntity>();
+
+        public async Task<IQueryable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? expression = null,
+            Expression<Func<TEntity, object>>? oredrByExpression = null, 
+            bool IsDescending = false, 
+            params string[] includes)
+        {
+            IQueryable<TEntity> query = Table.Where(e=>!e.IsDeleted);
+            if (includes is not null)
+            {
+                for (int i = 0; i < includes.Length; i++)
+                {
+                    query = query.Include(includes[i]);
+                }
+            }
+            if (oredrByExpression != null)
+            {
+                query = IsDescending ? query.OrderByDescending(oredrByExpression) :
+                    query.OrderBy(oredrByExpression);
+            }
+            if (expression is not null)
+            {
+                query = query.Where(expression);
+            }
+            return query;
+        }
+    }
+}
